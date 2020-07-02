4402441303bbe63eb891e1faf5d54a1ee9fcbbbc
Laura <viglionigr@gmail.com>
2020-07-01 22:51:04 -0300

    small husky configs

--- src/basis.js
+++ src/basis.js
@@ -3,15 +3,15 @@ const { randArr, randIntArr } = require('./random')
 const { map } = require('ramda')
 const { emptyArray } = require('./array')
 
-const generateBasis = (type) => (n, min = -100, max = 100) => {
+const generateBasis = (type) => (rank, min = -100, max = 100) => {
   const randGen = {
     int: randIntArr,
     float: randArr,
   }[type]
-  const vectors = map(() => randGen(n, min, max), emptyArray(n))
+  const vectors = map(() => randGen(rank, min, max), emptyArray(rank))
   const basis = matrix(vectors)
   if (determinant(basis) !== 0) return basis
-  else return generateBasis(type)(n, min, max)
+  else return generateBasis(type)(rank, min, max)
 }
 
 const genIntBasis = generateBasis('int')

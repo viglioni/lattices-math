c78ceb0c5f0d161a7b56d8a4da29d7274db781aa
Laura <viglionigr@gmail.com>
2020-07-03 13:46:08 -0300

    finished projection tests

--- src/gram-schmidt.test.js
+++ src/gram-schmidt.test.js
@@ -1,22 +1,42 @@
-const { equals, map, apply } = require('ramda')
+const { equals, map, apply, unapply, forEach } = require('ramda')
 const { projection } = require('./gram-schmidt')
 const { fraction } = require('mathjs')
 
 test('should return 0 vector when u is 0', () => {
-  const proj = projection([0])([1, 2, 3, 4, 5])
-  expect(equals(proj, [0])).toBeTruthy()
+  const proj = projection([0, 0, 0, 0, 0])([1, 2, 3, 4, 5])
+  expect(equals(proj, [0, 0, 0, 0, 0])).toBeTruthy()
 })
 
 test('should return correct values', () => {
-  const testHelper = (u, v, result) => {
+  const testHelper = apply((u, v, result) => {
     expect(equals(projection(u)(v), map(apply(fraction), result))).toBeTruthy()
-  }
-  testHelper(
-    [1, 2],
-    [3, 4],
+  })
+  const valuesToTest = [
     [
-      [11, 5],
-      [22, 5],
+      [1, 2],
+      [3, 4],
+      [
+        [11, 5],
+        [22, 5],
+      ],
     ],
-  )
+    [
+      [1, 2, -10, 15, 18],
+      [33, 0, -12, 3, 4],
+      [
+        [45, 109],
+        [90, 109],
+        [-450, 109],
+        [675, 109],
+        [810, 109],
+      ],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+      [[0], [0], [0], [0], [0], [0], [1], [0], [0], [0]],
+    ],
+  ]
+
+  forEach(testHelper, valuesToTest)
 })

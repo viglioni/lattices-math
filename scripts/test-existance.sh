2a882660b10513945e7fc7a2129bd28ec12bd4bf
Laura <viglionigr@gmail.com>
2020-07-05 15:23:53 -0300

    added tsconfig and adapted test script

--- scripts/test-existance.sh
+++ scripts/test-existance.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 find_code_files () {
-    find . -regex '^./src/[^index][a-z0-9\-]*\.js'
+  find . -regex '^\./src/[^index][a-z0-9\/\-]*\.[tj]s'
 }
 
 find_test_files () {
-    echo $@ | sed 's/\.js/.test.js/g'
+  echo $@ | sed -e 's/\.js/.test.js/g' -e 's/\.ts/.test.ts/g'
 }
 
 
 main (){ 
-    code_files=$(find_code_files)
-    test_files=$(find_test_files $code_files)
+  code_files=$(find_code_files)
+  test_files=$(find_test_files $code_files)
 
-    ret=true
-    for file in $test_files
-    do
-        if test -f $file
-        then true
-        else
-            ret=false
-            echo $( echo $file | sed 's/\.test//') does not have a test file!
-        fi
-    done
+  ret=true
+  for file in $test_files
+  do
+    if test -f $file
+    then true
+    else
+      ret=false
+      echo $( echo $file | sed 's/\.test//') does not have a test file!
+    fi
+  done
 
-    $ret
+  $ret
 }
 
 main
-

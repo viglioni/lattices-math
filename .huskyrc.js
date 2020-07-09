975df7313f40ed78439054056fc486d0a1f7b537
Laura <viglionigr@gmail.com>
2020-07-08 23:57:19 -0300

    changed the scripts order in husky

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,16 +3,15 @@ const tasks = arr => arr.join(' && ')
 module.exports = {
   'hooks': {
     'pre-commit': tasks([
+      'sh ./scripts/generate-index.sh',
       'sh ./scripts/test-existance.sh',
       'sh ./scripts/no-js-allowed.sh',
-      'sh ./scripts/generate-index.sh'
     ]),
     'pre-push': tasks([
       "sh ./scripts/no-js-allowed.sh",
       "sh ./scripts/test-existance.sh",
       "yarn test",
       "lint-staged",
-      'sh ./scripts/generate-index.sh'
     ])
   }
 }

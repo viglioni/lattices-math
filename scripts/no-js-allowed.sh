e00c578d24a3ac58c7507ee29699ec6a2026508a
Laura <viglionigr@gmail.com>
2020-07-08 21:29:08 -0300

    added js checker

--- scripts/no-js-allowed.sh
+++ scripts/no-js-allowed.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 find_js () {
-  find . -regex '^\.\/src/.*\.ts$'
+  find . -regex '^\.\/src/.*\.js$'
 }
 
 check_js_existance () {
-  [$(find_js) -eq ''] && true || false
+  js_files=$(find_js)
+  if [$js_files -eq '']
+  then true
+  else (echo "There are js files in ./src! \n $js_files \n Please use typescript" && false)
+  fi
 }
+
+
+check_js_existance

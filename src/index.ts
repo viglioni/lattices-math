28c65741f07adbc8d3b53088cb7e08213491621a
Laura <viglionigr@gmail.com>
2020-07-08 22:46:10 -0300

    added rationals to index

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import * as vector from './vector'
 import * as basis from './basis'
 import * as gram from './gram-schmidt'
 import * as random from './random'
+import * as rational from './rational'
 
 export default {
   ...vector,
   ...basis,
   ...gram,
   ...random,
+  ...rational,
 }
